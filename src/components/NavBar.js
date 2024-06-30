238f074ccf77fe6a4b3cf01227774e16311373fb
Anand chaurasia <151776571+AnandCh20@users.noreply.github.com>
2024-06-30 16:13:41 +0530

    NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -14,13 +14,13 @@ const NavBar = ({ cartItems, removeFromCart, numOfItems, toggleCart, showProduct
   };
 
   return (
-    <nav className="navbar navbar-expand-md px-4 bg-dark" data-bs-theme="dark" style={{ width: "100%", border: "2px solid blue" }}>
+    <nav className="navbar navbar-expand-md px-4 bg-dark" data-bs-theme="dark" style={{ width: "100%" }}>
       <h1 className="navbar-brand text-white" onClick={handleShowProductsClick}>My_Shop</h1>
       <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvas" aria-controls="offcanvas" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
 
-      <div className={`offcanvas offcanvas-end ${cartOpen ? 'show' : ''}`} tabIndex="-1" id="offcanvas" aria-labelledby="offcanvasLabel">
+      <div className={`offcanvas offcanvas-end ${cartOpen ? 'show' : ''}` } tabIndex="0" id="offcanvas" aria-labelledby="offcanvasLabel"  style={{ width: "50%"}}>
         <div className="offcanvas-header">
           <h5 className="offcanvas-title" id="offcanvasLabel">My_Shop</h5>
           <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close" onClick={handleCartClick}></button>
@@ -31,8 +31,8 @@ const NavBar = ({ cartItems, removeFromCart, numOfItems, toggleCart, showProduct
             <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
           </form>
           <div className="navbar-nav flex-grow-1 justify-content-end">
-            <button className="rounded-2"><a className="nav-link" href="#">Support</a></button>
-            <button className="rounded-2"><a className="nav-link" href="#" onClick={handleCartClick}>🛒Cart ({numOfItems})</a></button>
+            <button className="rounded-2">Support</button>
+            <button className="rounded-2" onClick={handleCartClick}>🛒Cart ({numOfItems})</button>
           </div>
         </div>
       </div>
